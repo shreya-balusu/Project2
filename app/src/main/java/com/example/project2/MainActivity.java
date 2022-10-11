f0731b7ec9df4e5d2fd96b85677a2d096f8b0e67
Wayland Fong <waylandf@usc.edu>
2022-10-11 16:25:00 -0700

    Initial commit

---
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        EditText editView = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editView.getText().toString();
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra("com.example.sendmessage.MESSAGE", message);
+
+        startActivity(intent);
+    }
+
+}
